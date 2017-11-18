94c6bc5a995305aac2354d39fc88e51e95bad198
Miles <Milesvbowles@gmail.com>
2017-11-18 13:07:17 -0800

    Added template code for home.html

--- server.js
+++ server.js
@@ -32,14 +32,6 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.get("/make", function(req, res) {
-    res.sendFile(path.join(__dirname, "make.html"));
-});
-
-app.get("/view", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
-});
-
 app.get("/test", function(req, res) {
     res.json(reservations);
 });
